f5ee61372f46cc027e01f52c9f188fd6c9a80060
Michael Ehlmane <ehlma1md@cmich.edu>
2026-05-04 23:09:38 -0400

    changes

---
+++ lab12/js/script.js
@@ -0,0 +1,11 @@
+console.log("JavaScript is connected!");
+
+// Open the overlay
+function openOverlay () {
+    document.getElementById("overlay-menu").style.display="flex";
+
+}
+
+function closeOverlay(){
+    document.getElementById ("")
+}
